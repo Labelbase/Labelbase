39686b58074d87f738b5deeee3fcd2691fc91747
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-03 18:38:47 +0100

    .

--- labelbase/forms.py
+++ labelbase/forms.py
@@ -1,9 +1,16 @@
 
-from django.forms import ModelForm
+from django import forms
+from .models import Label, Labelbase
 
-from .models import Label
-
-class LabelForm(ModelForm):
+class LabelForm(forms.ModelForm):
     class Meta:
         model = Label
         fields = ['labelbase', 'type', 'ref', 'label']
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request')
+        super(LabelForm, self).__init__(*args, **kwargs)
+        self.fields['labelbase'] = forms.ModelChoiceField(
+            queryset=Labelbase.objects.filter(user_id=request.user.id),
+            required=True,
+            widget=forms.Select(attrs={'class': 'chzn-select'}),
+        )

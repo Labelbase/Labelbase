e6fac98e09a11994031d8cd99094b3a46da698a5
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-03 13:53:38 +0100

    .

--- labellabor/settings.py
+++ labellabor/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     #'two_factor.plugins.yubikey',  # <- for yubikey capability.
 
     'labelbase',
+    'userprofile',
 
     'debug_toolbar',
     'bootstrapform'

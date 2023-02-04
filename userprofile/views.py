bf1c0f626376bb47a585adecf903a63b81c62080
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-04 20:19:50 +0100

    .

--- userprofile/views.py
+++ userprofile/views.py
@@ -13,6 +13,6 @@ class APIKeyView(TemplateView):
     template_name = 'apikey.html'
 
     def get_context_data(self, **kwargs):
-        context = super(ProfileView, self).get_context_data(**kwargs)
+        context = super(APIKeyView, self).get_context_data(**kwargs)
         context['api_token'] = Token.objects.get(user_id=self.request.user.id)
         return context

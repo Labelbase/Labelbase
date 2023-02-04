1c86cac41ee14312e819ac9ae7773a7710c1a122
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-04 23:04:40 +0100

    .

--- labelbase/templatetags/labelbase_tags.py
+++ labelbase/templatetags/labelbase_tags.py
@@ -4,5 +4,5 @@ from labelbase.forms import LabelbaseForm
 register = template.Library()
 
 @register.simple_tag
-def labelbaseform():
-    return LabelbaseForm()
+def labelbaseform(labelbase=None):
+    return LabelbaseForm(instance=labelbase)

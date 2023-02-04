4054320dab5381dfb1f80d46b3c5d45632b6c378
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-04 09:38:05 +0100

    .

--- labellabor/urls.py
+++ labellabor/urls.py
@@ -6,7 +6,7 @@ from django.urls import include, path
 from two_factor.urls import urlpatterns as tf_urls
 
 from .views import (
-    ExampleSecretView, LabelbaseView, HomeView, RegistrationCompleteView, RegistrationView,
+    ExampleSecretView, LabelbaseView, HomeView, RegistrationCompleteView, RegistrationView
 )
 
 from userprofile.views import ProfileView
@@ -58,8 +58,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
+ 

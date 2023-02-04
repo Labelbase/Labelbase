de318e7e18eff566e2c8fc7809df6f66fd2ab88c
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-04 18:58:27 +0100

    .

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import  TemplateView # FormView,
+from rest_framework.authtoken.models import Token
 
 class ProfileView(TemplateView):
+    """ """
     template_name = 'profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileView, self).get_context_data(**kwargs)
+        context['api_token'] = Token.objects.get(user_id=self.request.user.id)
+        return context

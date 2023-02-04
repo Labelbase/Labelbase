b4857b4f2d4d27b13518792e7538cef729b74779
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-04 10:08:01 +0100

    .

--- two_factor/plugins/email/__init__.py
+++
@@ -1,4 +0,0 @@
-import django
-
-if django.VERSION <= (3, 2):
-    default_app_config = 'two_factor.plugins.email.apps.TwoFactorEmailConfig'
--- two_factor/signals.py
+++
@@ -1,4 +0,0 @@
-from django.dispatch import Signal
-
-# Signal additional parameters are: request, user, and device.
-user_verified = Signal()

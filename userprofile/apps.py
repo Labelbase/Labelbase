8de8c1c4682e8aec1fd8c6d17429f1763e13d85d
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-04 11:02:17 +0100

    .

--- userprofile/apps.py
+++ userprofile/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class UserprofileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userprofile'
-    
+
     def ready(self):
-        import users.signals
+        import .signals

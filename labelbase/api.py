0421fd3da18411d9aa9edb9c0203c686567c97f8
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-08 13:57:20 +0100

    .

--- labelbase/api.py
+++ labelbase/api.py
@@ -22,18 +22,18 @@ class LabelSerializer(serializers.ModelSerializer):
         #exclude = ['labelbase', ]
 
 class LabelbaseSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Labelbase
         fields = ['id', 'name', 'fingerprint', 'about', 'user', ]
         read_only_fields = ['id', 'user', ]
         #exclude = ['user']
 
-
-
     """def create(self, validated_data):
         print(validated_data)
         obj = Labelbase(**validated_data)
-        obj.user_id = validated_data.get('user') # CurrentUserDefault()
+        obj.user_id = CurrentUserDefault()
         obj.save()
         return obj"""
 

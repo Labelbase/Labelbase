95af8d343a49ec2b6a964973fd6d29ac5f766951
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-09 15:23:50 +0100

    .

--- importer/forms.py
+++ importer/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 
+IMPORT_CHOICES = (
+    ('BIP-0329', 'BIP-0329'),
+    ('csv-bluewallet', 'BlueWallet .csv'),
+)
+
+
 class UploadFileForm(forms.Form):
     labelbase_id = forms.IntegerField()
+    import_type = forms.ChoiceField(choices = IMPORT_CHOICES)
     file = forms.FileField()

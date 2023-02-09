c7a532f358b170a07aa500047d5e8c6517661da9
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-09 20:10:46 +0100

    .

--- importer/forms.py
+++ importer/forms.py
@@ -1,18 +1,12 @@
 from django import forms
 
-IMPORT_CHOICES = (
+IMPORTER_CHOICES = (
     ('BIP-0329', 'BIP-0329'),
     ('csv-bluewallet', 'BlueWallet .csv'),
 )
 
 
 class UploadFileForm(forms.Form):
-    labelbase_id = forms.IntegerField()
-    import_type = forms.ChoiceField(choices = IMPORT_CHOICES, widget = forms.HiddenInput())
+    labelbase_id = forms.IntegerField(widget=forms.HiddenInput())
+    import_type = forms.ChoiceField(choices=IMPORTER_CHOICES, widget=forms.HiddenInput())
     file = forms.FileField()
-
-    def __init__(self, *args, **kwargs):
-        super(UploadFileForm, self).__init__(*args, **kwargs)
-        #labelbase_id = kwargs.get('labelbase_id', None)
-        #if labelbase_id:
-        #    self.fields['labelbase_id'].initial = labelbase_id
--- importer/templatetags/importer_tags.py
+++ importer/templatetags/importer_tags.py
@@ -5,6 +5,7 @@ register = template.Library()
 
 @register.simple_tag
 def bip0329labeluploadform(labelbase_id):
-    form =  UploadFileForm()#labelbase_id=labelbase_id)
+    form =  UploadFileForm()
     form.fields['labelbase_id'].initial = labelbase_id
+    form.fields['import_type'].initial = 'BIP-0329'
     return form

af8a9c5de00d5a23d97c3d2818266878cf94683b
Xavier Fiechter <xavierfiechter@gmail.com>
2023-02-09 19:06:50 +0100

    .

--- importer/templatetags/importer_tags.py
+++ importer/templatetags/importer_tags.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.simple_tag
 def bip0329labeluploadform(labelbase_id):
-    return UploadFileForm(labelbase_id=labelbase_id, format="bip0329")
+    return UploadFileForm()
